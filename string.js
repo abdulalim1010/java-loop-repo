88205fa50617674da9930f3c65a7c02391797f68
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-09 12:20:34 +0600

    odd and even number

---
+++ string.js
@@ -0,0 +1,13 @@
+/* 
+let n = 80;
+for (n = 41; n <= 80; n++){
+  if (n % 2 == 1)
+  {
+    console.log(n);
+  }
+} */
+for (let i = 60; i <= 100; i++){
+  if (i % 2 == 0) {
+    console.log(i)
+  }
+}
