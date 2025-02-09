1c7e42176e05e5f7f0c363c3560d2021abaa6f18
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-09 11:24:21 +0600

    wrong time

---
+++ while.js
@@ -0,0 +1,16 @@
+let n = 100;
+let sum = 0;
+while (n <= 200) {
+  if (n % 2 == 0) {
+    sum = sum + n;
+  }
+    n++;
+  if (sum >= 200) {
+    console.log(sum);
+      break;
+       
+    }
+   
+  }
+
+  
